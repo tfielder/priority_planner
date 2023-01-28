717b04aa0e57cea370bbec4f0d22d9434a3af5f4
Tim Fielder <timfielderdev@gmail.com>
2023-01-28 11:52:05 -0700

    adjust routes to new main controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'main/index'
-  get 'main/about'
+  root 'main#index'
+  get '/about', to: 'main#about'
   resources :collections
   resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
